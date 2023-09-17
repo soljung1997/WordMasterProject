a665afd330e4325acb44555c87489025948293f2
solomon <sol.jung1997@gmail.com>
2023-09-17 17:48:16 +0900

    Update 6
    단어 전에 whitespace 제거 (" apple" -> "apple" 로 변경)

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -2,6 +2,8 @@ package com.mycom.word;
 import java.io.*;
 import java.util.ArrayList ;
 import java.util.* ;
+import java.util.logging.Logger;
+
 public class WordCRUD implements ICRUD {
     Scanner s ;
     final String fname = "Dictionary.txt" ;
@@ -129,7 +131,7 @@ public class WordCRUD implements ICRUD {
             br.close() ;
             System.out.println("==> " + count + "개 로딩 완료!!!") ;
         } catch(IOException e){
-            e.printStackTrace() ;
+            e.printStackTrace();
         }
     }
 
